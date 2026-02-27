82baeaf624ad87411311c6886fce50f391763616
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-02-27 15:06:34 +0100

    Update OrderServer.jsx

--- src/pages/OrderServer.jsx
+++ src/pages/OrderServer.jsx
@@ -24,16 +24,18 @@ export default function OrderServer() {
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-console.log("SERVER TEMPLATE:", import.meta.env.VITE_EMAILJS_SERVER_TEMPLATE_ID);
+
+  console.log("TEMPLATE:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+
   try {
     await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_SERVER_TEMPLATE_ID, // <-- new template for server orders
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
       {
         name: form.name,
         discord: form.discord,
         email: form.email,
-        serverType: form.serverType,
+        botType: `SERVER-${form.serverType}`, // <--- change this line
         description: form.description,
         reply_to: form.email,
       },
